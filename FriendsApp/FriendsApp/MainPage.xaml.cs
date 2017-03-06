670ef57bec48395f4b0c49f095ad085b6c54353e
Greis <mort5240@gmail.com>
2017-03-06 13:58:42 +0100

    Friend model created. Simple display functionallity to MainPage added.

--- FriendsApp/FriendsApp/MainPage.xaml.cs
+++ FriendsApp/FriendsApp/MainPage.xaml.cs
@@ -3,15 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using FriendsApp.Models;
 using Xamarin.Forms;
 
 namespace FriendsApp
 {
     public partial class MainPage : ContentPage
     {
+        private List<Friend> Friends;
+
         public MainPage()
         {
             InitializeComponent();
+            InitDummyData();
+            CreateFriendsList();
+            FriendsCount.Text = Friends.Count.ToString();
+        }
+
+        private void InitDummyData()
+        {
+            Friends = new List<Friend>();
+
+            Friends.Add(new Friend() { Name = "Kaj", Address = "Kajsvej 8", Email = "kaj@kaj.dk", PhoneNr = "69696969", ImageUrl = "power.png" });
+            Friends.Add(new Friend() { Name = "Kaj", Address = "Kajsvej 8", Email = "kaj@kaj.dk", PhoneNr = "69696969", ImageUrl = "power.png" });
+        }
+
+        //This method generates the XAML needed to fill the FriendList.
+        private void CreateFriendsList()
+        {
+            FriendsList.ItemsSource = Friends;
         }
     }
 }
---
+++ FriendsApp/FriendsApp/Models/Friend.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FriendsApp.Models
+{
+    public class Friend
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PhoneNr { get; set; }
+        public string Email { get; set; }
+        public string ImageUrl { get; set; }
+        public string GpsLocation { get; set; }
+        public string HomepageUrl { get; set; }
+
+        public Friend()
+        {
+            
+        }
+    }
+
+
+}
